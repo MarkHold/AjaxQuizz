57651c122af811349b0d8bcaf1b245bf8a92198a
markuslyconhold <ma223ik@student.lnu.se>
2016-11-05 19:30:47 +0100

    fixed the starting form, showing and hiding divs and more.

--- examination/client/source/js/app.js
+++ examination/client/source/js/app.js
@@ -7,11 +7,14 @@ var questions = function () {
 
   var questionButton = document.getElementById("getQuestionButton");
   var startButton = document.getElementById("startButton");
+  var FunctionTestButton = document.getElementById("hidebutton");
 
   var currentPlayerButton = document.getElementById("UserNameButton");
   var currentPlayerStatus = document.getElementById("currentplayer");
+  var AnswerArea = document.getElementById("AnswerArea");
+  var RadioArea = document.getElementById("radioplace");
 
-  var StartURL = "http://vhost3.lnu.se:20080/question/1";
+  var StartURL = "http://vhost3.lnu.se:20080/question/21";
   var questionLink = document.getElementById("Q1");
   var questAltLink = document.getElementById("Qalt");
 
@@ -25,6 +28,13 @@ var questions = function () {
   var nextLink;
   var Answer;
 
+  document.getElementById("Questions").style.visibility = "hidden";
+  document.getElementById("radioplace").style.visibility = "hidden";
+  document.getElementById("timerName").style.visibility = "hidden";
+  document.getElementById("getQuestionButton").style.visibility = "hidden";
+  document.getElementById("AnswerArea").style.visibility = "hidden";
+  document.getElementById("startButton").style.visibility = "hidden";
+  document.getElementById("time").style.visibility = "hidden";
 
 
   function QuestionGame(RequestType, Answer){
@@ -45,11 +55,25 @@ var questions = function () {
 
         // questAltLink.innerText = response.alternatives;
 
+         if(response.alternatives !=null){
+           questAltLink.innerText = response.alternatives;
+           AnswerArea.style.visibility = "hidden";
+           RadioArea.style.visibility = "visible";
+          }
+
+        HideAllRadios();
+        ShowArea();
+
+        if(response.nextURL == undefined) {
+          countDown = 0;
+
+        }
       }
 
 
-      if(nextLink === undefined){
-        //quizz done function
+      if(response.message == "Correct answer!"){
+        questionLink.innerText = "Correct Answer! Get the next question";
+        countDown = 20;
       }
 
 
@@ -70,20 +94,76 @@ var questions = function () {
 }
 
 
+  FunctionTestButton.onclick = function () {
+
+    document.getElementById("Questions").style.visibility = "hidden";
+    document.getElementById("radioplace").style.visibility = "hidden";
+    document.getElementById("timerName").style.visibility = "hidden";
+    document.getElementById("getQuestionButton").style.visibility = "hidden";
+    document.getElementById("currentPlayerName").style.visibility = "hidden";
+    document.getElementById("AnswerArea").style.visibility = "hidden";
+    document.getElementById("startButton").style.visibility = "hidden";
+    document.getElementById("time").style.visibility = "hidden";
+
+
+  }
+  function HideArea(){
+    document.getElementById("AnswerArea").style.visibility = "hidden";
+  }
+  function ShowArea(){
+    document.getElementById("AnswerArea").style.visibility = "visible";
+
+  }
+  function ShowAllRadios(){
+    document.getElementById("radioplace").style.visibility = "visible";
+  }
+
+  function HideAllRadios(){
+    document.getElementById("radioplace").style.visibility = "hidden";
+  }
+
+  function ShowRadioFour(){
+    document.getElementById("alt4").style.visibility = "visible";
+  }
+
+  function HideRadioFour(){
+    document.getElementById("alt4").style.visibility = "hidden";
+  }
+
   currentPlayerButton.onclick = function(){
+   //Hides everything that has nothing to do with current player name.
     var currentPlayer = document.getElementById("UserNames").value;
     document.getElementById("currentplayer").innerHTML = currentPlayer;
 
+    document.getElementById("Questions").style.visibility = "visible";
+    document.getElementById("radioplace").style.visibility = "visible";
+    document.getElementById("timerName").style.visibility = "visible";
+    document.getElementById("getQuestionButton").style.visibility = "visible";
+    document.getElementById("currentPlayerName").style.visibility = "visible";
+    document.getElementById("AnswerArea").style.visibility = "visible";
+    document.getElementById("startButton").style.visibility = "visible";
+    document.getElementById("time").style.visibility = "visible";
+    document.getElementById("radioplace").style.visibility = "hidden";
+
+
+    document.getElementById("UserNameButton").style.visibility = "hidden";
+    document.getElementById("UserNames").style.visibility = "hidden";
+    document.getElementById("UserNameTitle").style.visibility = "hidden";
+
+
+
+
   }
 
+
   startButton.onclick = function(){
+    HideAllRadios();
     QuestionGame("GET", null);
     countDown = 20;
   }
 
 
   questionButton.addEventListener("click", function () {
-    console.log(url);
     var Answer = document.getElementById("AnswerArea").value;
     QuestionGame("POST", Answer);
 
@@ -106,7 +186,7 @@ var questions = function () {
   }
 
 
-
+/*
 
   //Section for the total time function
 
@@ -147,8 +227,7 @@ var questions = function () {
       }, 100);
     }
   };
-
-
+*/
 };
 
 questions();
